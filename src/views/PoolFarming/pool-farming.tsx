96d6c865e501b3d9807fb10d3479963bc3b36d09
sameretto <samer603m@yahoo.com>
2022-01-29 13:00:19 -0500

    only one mint token allowance needed to mint

--- src/views/PoolFarming/pool-farming.tsx
+++ src/views/PoolFarming/pool-farming.tsx
@@ -601,7 +601,7 @@ export default function PoolFarming({ theme }: any) {
               className="stake-button"
               variant="contained"
               color="primary"
-              disabled={isLoading || !hasDaiAllowance() || !hasUsdcAllowance() || !inWhitelist()}
+              disabled={isLoading || (hasDaiAllowance() || hasUsdcAllowance() ? false : true) || !inWhitelist()}
               onClick={() => onUserAction("mint")}
             >
               Mint
